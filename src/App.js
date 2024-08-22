d51ca78f8b356560c29490153e4647adb84cfbed
bernice uwituze <b.uwituze@alustudent.com>
2024-08-22 22:54:03 +0200

    project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+
+function App() {
+  return (
+    <>
+
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+    
+    </>
+  );
+}
+
+export default App;
