0c1e107679c8ce3d8d583a30409fa34c01fb7efc
bernice uwituze <b.uwituze@alustudent.com>
2024-08-26 14:42:58 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import '../src/styles/home/home.css';
 import Courses from './pages/coursesPage';
+import Register from './components/Register';
+import Login from './components/Login';
+import LiveCourse from './components/LiveCourse';
+import LiveDetails from './components/LiveDetails'
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/courses" element={<Courses />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/liveDetails" element={<LiveDetails />} />
+          <Route path="/liveCourse" element={<LiveCourse />} />
         </Routes>
       </Router>
     
@@ -19,28 +27,5 @@ function App() {
 }
 
 export default App;
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Register from './components/Register';
-import Login from './components/Login';
-import LiveCourse from './components/LiveCourse';
-import LiveDetails from './components/LiveDetails'
-import Footer from './components/Footer'
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/liveDetails" element={<LiveDetails />} />
-          <Route path="/liveCourse" element={<LiveCourse />} />
-          <Route path="/" element={<Login />} /> 
-        </Routes>
-        <Footer/>
-      </div>
-    </Router>
-  );
-}
 
-export default App;
