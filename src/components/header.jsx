fc35d22b076dc960d71e98393f1ab05893579295
bernice uwituze <b.uwituze@alustudent.com>
2024-08-26 10:02:14 +0200

    fix input

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../styles/components/header.css';
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return(
@@ -9,17 +10,17 @@ const Header = () => {
                 <div className="logo">
                     <img src="/images/logo.svg" alt="logo" />
                 </div>
+    
                 <div className="navbar">
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Courses</a></li>
-                        <li><a href="#">Mentorship</a></li>
-                        <li><a href="#">Contact</a></li>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/courses'>Courses</Link></li>
+                        <li><Link to='/live-class'>Live Classes</Link></li>
                     </ul>
                 </div>
                 <div className="login-signup">
-                    <a href="#" className="button login">Login</a>
-                    <a href="#" className="button signup">Sign Up</a>
+                    <Link to='/login' className="button login">Login</Link>
+                    <Link to='/register' className="button signup">Sign Up</Link>
                 </div>
             </div>
         </div>
