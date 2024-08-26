b404aa114f206e6f8af6283688db6d147ab95f91
bernice uwituze <b.uwituze@alustudent.com>
2024-08-26 09:24:19 +0200

    fix homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import '../src/styles/home/home.css';
+import Courses from './pages/coursesPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/courses" element={<Courses />} />
         </Routes>
       </Router>
     
